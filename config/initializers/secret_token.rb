63427ff5bfed5b4e5c28e3ace894f07c6f438058
Magdalena Szczerbowska <mszczerbowska@neubloc.net>
2011-12-20 14:21:11 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbSearch::Application.config.secret_token = '07105a3fb4dcc33f537a498ca0291dd93f336af5fdf149f30fbee7f3c41d68389d68c22aac17513bd4a25211730553e3cb25cccf0ce4b22dee337ec9f84fa5a8'
